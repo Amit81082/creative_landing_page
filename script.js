f211a39ddf84dd12028fe958abebe929a8263a0e
Amit Maurya <amitkumarmaury10122005@gmail.com>
2025-04-05 16:18:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.textBox ', { origin: 'top' });
+ScrollReveal().reveal('.socialmedia, .imgBox', { origin: 'bottom' });
+ScrollReveal().reveal('.morepepsies', { origin: 'left' });
+// ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
